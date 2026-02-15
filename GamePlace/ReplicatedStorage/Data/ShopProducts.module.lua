0ceaa290c7881ae963858504942b0419ef5cd75f
ayoub ouahid <aouahid123@gmail.com>
2026-02-15 18:02:44 +0100

    add product ids

--- GamePlace/ReplicatedStorage/Data/ShopProducts.module.lua
+++ GamePlace/ReplicatedStorage/Data/ShopProducts.module.lua
@@ -18,31 +18,31 @@ local ShopProducts = {
             Order = 1,
             Products = {
                 {
-                    ProductId = 0,
+                    ProductId = 3537882222,
                     Cash = 3000,
                     Robux = 59,
                     DisplayName = "$3,000",
                 },
                 {
-                    ProductId = 0,
+                    ProductId = 3537882916,
                     Cash = 25000,
                     Robux = 379,
                     DisplayName = "$25,000",
                 },
                 {
-                    ProductId = 0,
+                    ProductId = 3537882920,
                     Cash = 100000,
                     Robux = 659,
                     DisplayName = "$100,000",
                 },
                 {
-                    ProductId = 0,
+                    ProductId = 3537882919,
                     Cash = 500000,
                     Robux = 1249,
                     DisplayName = "$500,000",
                 },
                 {
-                    ProductId = 0,
+                    ProductId = 3537884400,
                     Cash = 1000000,
                     Robux = 2499,
                     DisplayName = "$1,000,000",
